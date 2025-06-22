bcf991fb8d8ff72ccb7507d82235807acb952f1a
Daniel Picuasi <daniel.picuasi@centric.com.ec>
2025-06-21 21:41:38 -0500

    Se agrega modificaciones para deploy 3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'team/dev', pathMatch: 'full' },
-  { path: 'team/:teamId', loadComponent: () => import('./tablero/tablero.component').then(m => m.TableroComponent) }
+  { path: '', redirectTo: '/work-schedule-board/browser/team/dev', pathMatch: 'full' },
+  { path: '/work-schedule-board/browser/team/:teamId', loadComponent: () => import('./tablero/tablero.component').then(m => m.TableroComponent) }
 ];
